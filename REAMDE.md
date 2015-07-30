f750403f804314ad217051c332ba11b4b3244696
Assaf Arkin <assaf@labnotes.org>
2015-07-30 14:26:18 -0700

    First check in

---
+++ REAMDE.md
@@ -0,0 +1,176 @@
+# LRU cache based on ES6 Map
+
+An [LRU cache](http://www.cs.uml.edu/~jlu1/doc/codes/lruCache.html) that
+implements the same interface as [ES6
+Map](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map).
+
+You can set the cache limit when creating a new cache, or by setting the
+property `limit`.  If you set the limit to zero (or negative value), the cache
+will hold no keys.  If you set the limit to `Infinity` (or anything that's not a
+number) the cache will have no limit on how many keys it can hold.
+
+Otherwise, the cache will evict keys when it needs to make room for storing new
+keys.  It needs more room when the total cost of all, non-expired keys, plus the
+new key is bigger than the limit.
+
+For example, if the limit is set to 10, you can store 3 keys with a cost of 3 (3
+x 3 = 9), or 2 keys with a cost of 4 (3 x 4 = 12).  If you don't specify a cost
+when storing a key, the default is 1, which effectively makes it limit the
+number of keys stored.
+
+When the cache needs to make room for new keys, it evicts expired keys first,
+and then evicts the least recently used keys, but only enough to make room for
+the new key.
+
+Any time you get or set a key, it becomes the most recently used key.  Checking
+if a key exists (`has()`) does not affect the key's ranking.
+
+The `set` method takes the same two arguments (key and value), and a third
+argument which may have the following two options:
+
+- `cost` - The cost of this key, a positive number (0 is allowed), if absent the
+default is 1
+- `ttl` - The time to live in milliseconds, if absent the key never expires
+
+You can iterate over all keys in order of insertion using the `entries()`,
+`keys()` and `values()` iterators.  You can also iterate over all keys from most
+to least recent using the `recent()` iterator.  The default iterator is the
+`recent()` iterator.
+
+You can also initialize an LRU cache from an array, `Map` or any iterator with
+key/value pairs.  The recent order would be the reverse of the insertion order.
+And of course you can initialize an LRU cache from another cache, which would
+preserve all the same settings and order of keys.
+
+
+## Examples
+
+```js
+// This cache will hold at most two keys
+const two = new LRU(2);
+
+two.set('a', {});
+two.set('b', {});
+two.set('c', {});
+two.keys();
+=> [ 'b', 'c' ]
+
+
+// This cache will only hold keys with a total cost of 10, dropping least
+// recently used keys when it gets full
+const ten = new LRU(10);
+
+// 4+4+3=11 > 10 so will drop 'a'
+ten.set('a', '', { cost: 4 });
+ten.set('b', '', { cost: 4 });
+ten.set('c', '', { cost: 3 });
+ten.keys();
+=> [ 'b', 'c' ]
+
+// 'c' becomes least recently used
+ten.get('b');
+// 3+4+4=11 > 10 so will drop 'c'
+ten.set('d', '', { cost: 4 });
+ten.keys();
+=> [ 'b', 'd' ]
+
+// This key is too big to hold
+ten.set('too-big', '', { cost: 11 });
+ten.keys();
+=> [ 'b', 'd' ]
+
+
+// An easy way to disable caching, e.g. in development mode
+const empty = new LRU(0);
+empty.set('x', 1);
+empty.keys();
+=> []
+
+
+// LRU cache of infinite size
+// Useful if you want to evict keys only when they expire
+const infinite = new LRU(Infinity);
+
+// Will hold 'y' for a second, will not hold 'x'
+infinite.set('x', [], { ttl: 0 });
+infinite.set('y', [], { ttl: 1000 });
+infinite.keys();
+=> [ 'y' ]
+
+
+// New cache from map
+const fromMap = new Map(2, [ [ 'a', 1], [ 'b', 2 ] ]);
+fromMap.limit;
+=> 2
+fromMap.keys();
+=> [ 'a', 'b' ]
+
+// New cache from another cache
+const clone = new LRU(fromMap);
+clone.limit;
+=> 2
+clone.keys();
+=> [ 'a', 'b' ]
+```
+
+
+## Design
+
+ES6 introduces the `Map` class for holding key/value pairs.  The LRU cache uses
+the same interface, so you can use it the same way you could use a `Map`.
+
+`LRU` uses `Map` for its internal implementation, but does not extend it.  In
+the future, `Map` may gain additional methods and properties, and these may be
+"implemented" wrong if `LRU` automatically gained them via inheritence.
+
+`LRU` mostly preserves the same API as `Map` with the notable addition of the
+`limit` property, the `recent` iterator, and a 3rd argument to the `set` method.
+These are the minimum additions required for a working implementation.
+
+The `size` property is read-only so uses a getter.  The `limit` property is
+read/write, but we can't accept invalid values (e.g. `null` and "foo" don't make
+sense), so we use getter/setter to clean those up.
+
+Changing the limit does not evict any cached entries: there's an expectation
+that setting a property is a quick and low cost operation, and cache eviction is
+not.
+
+We use symbols for referencing internal properties, such as the head and tail
+links.  There is no reason for these to be accessed from outside the class.  We
+can also use symbols for internal methods (like prepending/removing links),
+although the shorthand syntax is not yet supported by io.js.  It is not a
+pressing issue, so we're using the underscore naming convention for now.
+
+Evicting old entries is an O(1) operation which makes it efficient, important
+for large caches under heavy use, but also means that expires keys are only
+removed as necessary to make room in the cache.  If you need to aggressively
+remove all expired keys, iterate over all of them or use a different LRU cache
+implementation.
+
+When setting a key, if the TTL is zero, or the cost is higher than the cache
+limit, the key is not stored.  This is because the key would have been evicted
+anyway, and there's no benefit in evicting the rest of the cache to make room
+for it.
+
+This also enables a zero limit cache that hold no keys.  Typically we want to
+cache in production but may not want to cache anything in development (so to
+dynmically reload when files change), and this can be done as easily as setting
+the cache limit based on the environment.
+
+The `has` method just checks if a key exists, it doesn't evict or change order,
+and so has no side effect of performance issues.  The `get` method does change
+the order, and so has a side effect, but one that's expected when using an LRU
+cache.
+
+The `set` method will evict as many keys as necessary, and so has side effects
+and may have a performance penalty, depending on number of keys that need to be
+evicted.  Though typically over a long period of time, it's an O(1) operation.
+
+Evicting all expired keys happens only when iterating over the cache, which is
+an O(N) operation.
+
+In Node, `console.log` uses the `inspect` method to print out complex objects,
+so we implement an `inspect` method that delegates to the `Map` to dump all the
+key/value pairs.  Since we store links in the map, we also implement an
+`inspect` method for each of them, that inspects the value itself.
+
---
+++ test.js
@@ -0,0 +1,650 @@
+/* eslint-env mocha */
+'use strict';
+const assert  = require('assert');
+const LRU     = require('./index');
+
+
+// Until we get Array.from(iterator) or [...iterator]
+function arrayFrom(iterator) {
+  const array = [];
+  for (let item of iterator)
+    array.push(item);
+  return array;
+}
+
+
+describe('LRU with limit of zero', function() {
+
+  const lru = new LRU(0);
+
+  it('should report a limit of zero', function() {
+    assert.equal(lru.limit, 0);
+  });
+
+
+  describe('after setting two keys', function() {
+
+    before(function() {
+      lru
+        .set('x', 'XXX')
+        .set('y', 'YYY');
+    });
+
+    it('should have a size of zero', function() {
+      assert.equal(lru.size, 0);
+    });
+
+    it('should have neither of these keys', function() {
+      assert.equal(lru.get('x'), undefined);
+      assert.equal(lru.get('y'), undefined);
+    });
+
+    it('should have no entries', function() {
+      assert.deepEqual(lru.entries().next(), { done: true, value: undefined });
+    });
+
+  });
+
+});
+
+
+describe('LRU with negative limit', function() {
+  const lru = new LRU(-10);
+
+  it('should report a limit of zero', function() {
+    assert.equal(lru.limit, 0);
+  });
+
+});
+
+
+describe('LRU with three keys', function() {
+
+  const lru = new LRU();
+
+  before(function() {
+    lru
+      .set('x', 'XXX')
+      .set('y', 'YYY')
+      .set('z', 'ZZZ');
+  });
+
+  it('should have a size of three', function() {
+    assert.equal(lru.size, 3);
+  });
+
+
+  describe('get', function() {
+
+    it('should return value of key', function() {
+      assert.equal(lru.get('x'), 'XXX');
+      assert.equal(lru.get('y'), 'YYY');
+      assert.equal(lru.get('z'), 'ZZZ');
+    });
+
+    it('should return undefined if key does not exist', function() {
+      assert.equal(lru.get('X'), undefined);
+      assert.equal(lru.get('a'), undefined);
+      assert.equal(lru.get(null), undefined);
+    });
+
+  });
+
+  describe('has', function() {
+
+    it('should return true if key exists', function() {
+      assert.equal(lru.has('x'), true);
+      assert.equal(lru.has('y'), true);
+      assert.equal(lru.has('z'), true);
+    });
+
+    it('should return false if key does not exist', function() {
+      assert.equal(lru.has('X'), false);
+      assert.equal(lru.has('a'), false);
+      assert.equal(lru.has(null), false);
+    });
+
+  });
+
+  describe('iterators', function() {
+
+    before(function() {
+      // This sets the LRU order to y -> z -> x
+      lru.get('z');
+      lru.get('y');
+    });
+
+    it('should iterate from most to least recent', function() {
+      const entries = arrayFrom( lru[Symbol.iterator]() );
+      assert.deepEqual(entries, [ [ 'y', 'YYY' ], [ 'z', 'ZZZ' ], [ 'x', 'XXX' ] ]);
+    });
+
+    it('should return all entries from most to least recent', function() {
+      const entries = arrayFrom( lru.entries() );
+      assert.deepEqual(entries, [ [ 'y', 'YYY' ], [ 'z', 'ZZZ' ], [ 'x', 'XXX' ] ]);
+    });
+
+    it('should return all keys from most to least recent', function() {
+      const keys = arrayFrom( lru.keys() );
+      assert.deepEqual(keys, [ 'y', 'z', 'x' ]);
+    });
+
+    it('should return all values from most to least recent', function() {
+      const values = arrayFrom( lru.values() );
+      assert.deepEqual(values, [ 'YYY', 'ZZZ', 'XXX' ]);
+    });
+
+  });
+
+  describe('forEach', function() {
+
+    before(function() {
+      // This sets the LRU order to y -> z -> x
+      lru.get('z');
+      lru.get('y');
+    });
+
+    it('should call with value as first argument (most to least recent)', function() {
+      const values = [];
+      lru.forEach(function(value) {
+        values.push(value);
+      });
+      assert.deepEqual(values, [ 'YYY', 'ZZZ', 'XXX' ]);
+    });
+
+    it('should call with key as second argument (most to least recent)', function() {
+      const keys = [];
+      lru.forEach(function(value, key) {
+        keys.push(key);
+      });
+      assert.deepEqual(keys, [ 'y', 'z', 'x' ]);
+    });
+
+    it('should call with LRU as third argument', function() {
+      lru.forEach(function(value, key, collection) {
+        assert.equal(collection, lru);
+      });
+    });
+
+    it('should call with this = thisArg', function() {
+      const thisArg = {};
+      lru.forEach(function() {
+        assert.equal(this, thisArg);
+      }, thisArg);
+    });
+
+    it('should call with this = undefined if no thisArg', function() {
+      lru.forEach(function() {
+        assert.equal(this, undefined);
+      });
+    });
+
+  });
+
+
+  describe('set existing key', function() {
+
+    before(function() {
+      lru
+       .set('x', '<x>')
+       .set('z', '<z>');
+    });
+
+    it('should not change map size', function() {
+      assert.equal(lru.size, 3);
+    });
+
+    it('should make last change the most recent entry', function() {
+      const entries = arrayFrom( lru );
+      assert.deepEqual(entries, [ [ 'z', '<z>' ], [ 'x', '<x>' ], [ 'y', 'YYY' ] ]);
+    });
+
+    it('should return new key value when asked for', function() {
+      assert.equal(lru.get('x'), '<x>');
+    });
+
+
+    describe('delete non-existing key', function() {
+
+      let returnValue;
+
+      before(function() {
+        lru.get('x');
+        lru.get('z');
+        returnValue = lru.delete('a');
+      });
+
+      it('should return true', function() {
+        assert.equal(returnValue, false);
+      });
+
+      it('should not change map size', function() {
+        assert.equal(lru.size, 3);
+      });
+
+      it('should keep all the same keys', function() {
+        const entries = arrayFrom( lru.keys() );
+        assert.deepEqual(entries, [ 'z', 'x', 'y' ]);
+      });
+
+    });
+
+
+    describe('delete existing key', function() {
+
+      let returnValue;
+
+      before(function() {
+        lru.get('x');
+        lru.get('y'); // move to head to list to test conditional when removing
+        returnValue = lru.delete('y');
+      });
+
+      it('should return true', function() {
+        assert.equal(returnValue, true);
+      });
+
+      it('should change map size', function() {
+        assert.equal(lru.size, 2);
+      });
+
+      it('should not return key when iterating', function() {
+        const entries = arrayFrom( lru );
+        assert.deepEqual(entries, [ [ 'x', '<x>' ], [ 'z', '<z>' ] ]);
+      });
+
+      it('should return undefined for key', function() {
+        assert.equal(lru.get('y'), undefined);
+      });
+
+      it('should return existing key value when asked for', function() {
+        assert.equal(lru.get('x'), '<x>');
+      });
+
+    });
+
+
+    describe('clear map', function() {
+      before(function() {
+        lru.clear();
+      });
+
+      it('should reset map size', function() {
+        assert.equal(lru.size, 0);
+      });
+
+      it('should have no keys', function() {
+        assert.equal(lru.has('x'), false);
+        assert.equal(lru.has('y'), false);
+        assert.equal(lru.has('z'), false);
+      });
+
+      it('should have no values', function() {
+        assert.equal(lru.get('x'), undefined);
+        assert.equal(lru.get('y'), undefined);
+        assert.equal(lru.get('z'), undefined);
+      });
+
+      it('should have no keys to iterate over', function() {
+        const keys = arrayFrom( lru.keys() );
+        assert.deepEqual(keys, []);
+      });
+
+      it('should have no entries to iterate over', function() {
+        const entries = arrayFrom( lru );
+        assert.deepEqual(entries, []);
+      });
+
+    });
+  });
+
+});
+
+
+describe('LRU with limit of five', function() {
+
+  const lru = new LRU(5);
+
+  it('should report a limit of five', function() {
+    assert.equal(lru.limit, 5);
+  });
+
+
+  describe('after adding six keys', function() {
+
+    before(function() {
+      lru
+        .set('a', 1)
+        .set('b', 2)
+        .set('c', 3)
+        .set('d', 4)
+        .set('x', '!')
+        .set('e', 5)
+        .set('f', 6);
+      lru.delete('x'); // exercise some linked list logic
+      lru.set('g', 7);
+    });
+
+    it('should hold 5 keys', function() {
+      assert.equal(lru.size, 5);
+    });
+
+    it('should not have first key', function() {
+      assert.equal(lru.has('a'), false);
+    });
+
+    it('should not have second key', function() {
+      // Dropped to make room for g
+      assert.equal(lru.has('b'), false);
+    });
+
+    it('should have third key', function() {
+      assert.equal(lru.has('c'), true);
+    });
+
+    it('should have seventh key', function() {
+      assert.equal(lru.has('g'), true);
+    });
+
+    it('should have five entries to iterate over', function() {
+      const entries = arrayFrom( lru );
+      assert.deepEqual(entries, [ [ 'g', 7 ], [ 'f', 6 ], [ 'e', 5 ], [ 'd', 4 ], [ 'c', 3 ] ]);
+    });
+
+
+    describe('set key with cost 3', function() {
+
+      before(function() {
+        lru.set('h', 8, { cost: 3 });
+      });
+
+      it('should hold 3 keys', function() {
+        assert.equal(lru.size, 3);
+      });
+
+      it('should drop two oldest keys', function() {
+        assert.equal(lru.has('c'), false);
+        assert.equal(lru.has('d'), false);
+      });
+
+      it('should have five entries to iterate over', function() {
+        const entries = arrayFrom( lru );
+        assert.deepEqual(entries, [ [ 'h', 8 ], [ 'g', 7 ], [ 'f', 6 ] ]);
+      });
+
+
+      describe('set key with cost 8', function() {
+
+        before(function() {
+          lru.set('i', 9, { cost: 8 });
+        });
+
+        it('should hold 3 keys', function() {
+          assert.equal(lru.size, 3);
+        });
+
+        it('should not add new key', function() {
+          assert.equal(lru.has('i'), false);
+        });
+
+        it('should keep all the same entries', function() {
+          const entries = arrayFrom( lru );
+          assert.deepEqual(entries, [ [ 'h', 8 ], [ 'g', 7 ], [ 'f', 6 ] ]);
+        });
+      });
+
+
+      describe('set another key with cost 3', function() {
+
+        before(function() {
+          lru.set('j', 10, { cost: 3 });
+        });
+
+        it('should hold one key', function() {
+          assert.equal(lru.size, 1);
+        });
+
+        it('should add new key', function() {
+          assert.equal(lru.has('j'), true);
+        });
+
+        it('should drop all other keys', function() {
+          const keys = arrayFrom( lru.keys() );
+          assert.deepEqual(keys, [ 'j' ]);
+        });
+
+      });
+
+    });
+
+  });
+
+});
+
+
+describe('LRU with no limit', function() {
+
+  const lru = new LRU();
+
+  it('should report a limit of Infinity', function() {
+    assert.equal(lru.limit, Infinity);
+  });
+
+
+  describe('after setting two large keys', function() {
+
+    before(function() {
+      lru
+        .set('x', 'XXX', { cost: Number.MAX_SAFE_INTEGER })
+        .set('y', 'YYY', { cost: Number.MAX_SAFE_INTEGER });
+    });
+
+    it('should have a size of two', function() {
+      assert.equal(lru.size, 2);
+    });
+
+    it('should retain both keys', function() {
+      assert.equal(lru.get('x'), 'XXX');
+      assert.equal(lru.get('y'), 'YYY');
+    });
+
+    it('should have both entries', function() {
+      const entries = arrayFrom( lru );
+      assert.deepEqual(entries, [ [ 'y', 'YYY' ], [ 'x', 'XXX' ] ]);
+    });
+
+  });
+
+});
+
+
+describe('Iterate and delete', function() {
+
+  const lru   = new LRU();
+  const keys  = [];
+
+  before(function() {
+    lru.set('x').set('y').set('z');
+
+    for (let key of lru.keys()) {
+      keys.push(key);
+      lru.delete(key);
+    }
+  });
+
+  it('should iterate over all keys from most to least recent', function() {
+    assert.deepEqual(keys, [ 'z', 'y', 'x' ]);
+  });
+
+  it('should delete all keys', function() {
+    assert.equal(lru.size, 0);
+  });
+
+});
+
+
+describe('LRU with expiring entries', function() {
+
+  const lru = new LRU(4);
+
+  before(function() {
+    // `a` expires immediately, never gets stored
+    lru
+      .set('a', 1, { ttl: 0 })
+      .set('b', 2, { ttl: 50 })
+      .set('c', 3, { ttl: 10 })
+      .set('d', 4);
+  });
+
+  before(function(done) {
+    // This will expire `c`
+    setTimeout(done, 10);
+  });
+
+  it('should not store immediately expired keys', function() {
+    assert.equal(lru.size, 3);
+  });
+
+
+  describe('expired key', function() {
+
+    it('should not have key', function() {
+      assert.equal(lru.has('a'), false);
+    });
+
+    it('should not have value', function() {
+      assert.equal(lru.get('a'), undefined);
+    });
+
+  });
+
+  describe('not yet expired key', function() {
+
+    it('should have key', function() {
+      assert.equal(lru.has('b'), true);
+    });
+
+    it('should have value', function() {
+      assert.equal(lru.get('b'), 2);
+    });
+
+  });
+
+
+  describe('iterators', function() {
+
+    before(function() {
+      assert.equal(lru.size, 3);
+    });
+
+    it('should skip expired keys', function() {
+      const keys = arrayFrom( lru.keys() );
+      assert.deepEqual(keys, [ 'b', 'd' ]);
+    });
+
+    it('should skip expired values', function() {
+      const values = arrayFrom( lru.values() );
+      assert.deepEqual(values, [ 2, 4 ]);
+    });
+
+    it('should skip expired entries', function() {
+      const entries = arrayFrom( lru.entries() );
+      assert.deepEqual(entries, [ [ 'b', 2 ], [ 'd', 4 ] ]);
+    });
+
+    it('should not forEach over expired entries', function() {
+      const keys = [];
+      lru.forEach(function(value, key) {
+        keys.push(key);
+      });
+      assert.deepEqual(keys, [ 'b', 'd' ]);
+    });
+
+    it('should also discard expired keys', function() {
+      assert.equal(lru.size, 2);
+    });
+
+  });
+
+
+  describe('set three keys', function() {
+
+    before(function() {
+      lru.get('d'); // enforce recent order
+      lru
+        .set('e', 5)
+        .set('f', 6)
+        .set('g', 7, { ttl: 0 });
+    });
+
+    it('should have a size of four', function() {
+      assert.equal(lru.size, 4);
+    });
+
+    it('should keep unexpired keys', function() {
+      assert.equal(lru.has('b'), true);
+      assert.equal(lru.has('d'), true);
+    });
+
+    it('should add new unexpired keys', function() {
+      assert.equal(lru.has('e'), true);
+      assert.equal(lru.has('f'), true);
+    });
+
+    it('should not add immediately expired keys', function() {
+      assert.equal(lru.has('g'), false);
+    });
+
+    it('should iterate over recent entries', function() {
+      const entries = arrayFrom( lru );
+      assert.deepEqual(entries, [ [ 'f', 6 ], [ 'e', 5 ], [ 'd', 4 ], [ 'b', 2 ] ]);
+    });
+
+  });
+
+});
+
+
+describe('Clone map', function() {
+
+  const map     = new Map([ [ 'a', 1 ], [ 'b', 2 ] ]);
+  const fromMap = new LRU(2, map);
+
+  it('should have the same keys as the source, but most to least recent', function() {
+    const keys = arrayFrom( fromMap.keys() );
+    assert.deepEqual(keys, [ 'b', 'a' ]);
+  });
+
+  it('should have the same values as the source, but most to least recent', function() {
+    const values = arrayFrom( fromMap.values() );
+    assert.deepEqual(values, [ 2, 1 ]);
+  });
+
+
+  describe('Clone cache', function() {
+
+    const fromCache = new LRU(fromMap);
+
+    it('should have the same limit as the source', function() {
+      assert.equal(fromCache.limit, 2);
+    });
+
+    it('should have same keys as the source, most to least recent', function() {
+      const keys = arrayFrom( fromCache.keys() );
+      assert.deepEqual(keys, [ 'b', 'a' ]);
+    });
+
+    it('should have same values as the source, most to least recent', function() {
+      const values = arrayFrom( fromCache.values() );
+      assert.deepEqual(values, [ 2, 1 ]);
+    });
+
+    it('should have the same cost as the source', function() {
+      fromCache.set('c');
+      assert.equal(fromCache.size, 2);
+    });
+
+  });
+
+});
+
